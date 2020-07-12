c6819492c9f1f60b57b9b1f9d8a45ee5c3749293
swonb <47047914+swonb@users.noreply.github.com>
2020-07-12 00:02:00 -0400

    Added Landing Component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Landing from "./components/Landing";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Landing />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +16,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
 });
---
+++ components/Landing.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { Button } from "native-base";
+
+const backGroundImg = require("../assets/images/landing.jpg");
+
+export default function Landing() {
+  return (
+    <ImageBackground source={backGroundImg} style={styles.background}>
+      <View style={styles.welcomeView}>
+        <Text style={styles.title}>Welcome to PokeSearch</Text>
+        <Button block={true} style={styles.button} onPress={() => {}}>
+          <Text style={styles.buttonText}>Start Searching</Text>
+        </Button>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  welcomeView: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 26,
+    color: "blue",
+    alignItems: "center",
+  },
+  button: {
+    margin: 10,
+  },
+  buttonText: {
+    color: "white",
+  },
+});

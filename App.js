b588d32c381df395197bbfa6ccf93f714555aa8f
swonb <47047914+swonb@users.noreply.github.com>
2020-07-12 00:12:22 -0400

    Added Screen Navigation

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import Constants from "expo-constants";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Landing from "./components/Landing";
+import Search from "./components/Search";
 
 export default function App() {
+  const [currentScreen, setCurrentScreen] = useState("landing");
+
+  const switchScreen = (screen) => {
+    setCurrentScreen(screen);
+  };
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case "search":
+        return <Search />;
+      case "landing":
+      default:
+        return <Landing switchScreen={() => switchScreen("search")} />;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Landing />
+      {renderScreen()}
       <StatusBar style="auto" />
     </View>
   );
--- components/Landing.js
+++ components/Landing.js
@@ -4,12 +4,12 @@ import { Button } from "native-base";
 
 const backGroundImg = require("../assets/images/landing.jpg");
 
-export default function Landing() {
+export default function Landing(props) {
   return (
     <ImageBackground source={backGroundImg} style={styles.background}>
       <View style={styles.welcomeView}>
         <Text style={styles.title}>Welcome to PokeSearch</Text>
-        <Button block={true} style={styles.button} onPress={() => {}}>
+        <Button block={true} style={styles.button} onPress={props.switchScreen}>
           <Text style={styles.buttonText}>Start Searching</Text>
         </Button>
       </View>

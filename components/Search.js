78eaf7c72e249ed12e6b1a357a5f127c1e0c0e65
swonb <47047914+swonb@users.noreply.github.com>
2020-07-13 00:19:38 -0400

    Added showing Search Result

--- components/Search.js
+++ components/Search.js
@@ -3,17 +3,31 @@ import { StyleSheet, Text, View } from "react-native";
 import { Header, Item, Icon, Input } from "native-base";
 import PokeLoader from "./PokeLoader";
 import SearchBody from "./SearchBody";
+import axios from "axios";
 
 export default function Search() {
   const [pokeSearch, setPokeSearch] = useState("");
   const [onCall, setOnCall] = useState(true);
+  const [data, setData] = useState({});
 
-  const searchPoke = () => {};
+  const searchPoke = () => {
+    setOnCall(true);
+    axios
+      .get("http://pokeapi.co/api/v2/pokemon/" + pokeSearch.toLowerCase())
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+        setOnCall(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   const renderBody = () => {
     if (onCall) {
       return <PokeLoader />;
     } else {
-      return <SearchBody />;
+      return <SearchBody data={data} />;
     }
   };
 
--- components/SearchBody.js
+++ components/SearchBody.js
@@ -1,12 +1,122 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+  Image,
+  FlatList,
+} from "react-native";
+import { ListItem, List } from "native-base";
 
-export default function SearchBody() {
+export default function SearchBody(props) {
+  const pokemon = props.data;
+
+  if (!pokemon) {
+    return <View />;
+  }
+
   return (
-    <View>
-      <Text>this is search data</Text>
+    // <ScrollView style={styles.container}>
+    //   <Text style={styles.header}>
+    //     #{pokemon.id} - {pokemon.name.toUpperCase()}
+    //   </Text>
+    //   <View style={styles.view}>
+    //     <Image
+    //       source={{ uri: pokemon.sprites.front_default }}
+    //       style={styles.image}
+    //     />
+    //   </View>
+    //   <View style={styles.info}>
+    //     <ListItem itemDivider>
+    //       <Text style={styles.infoText}>Size</Text>
+    //     </ListItem>
+    //     <ListItem>
+    //       <Text>Weight - {pokemon.weight}kg</Text>
+    //     </ListItem>
+    //     <ListItem>
+    //       <Text>Height - {pokemon.height / 10}m</Text>
+    //     </ListItem>
+    //     <ListItem itemDivider>
+    //       <Text style={styles.infoText}>Abilities</Text>
+    //     </ListItem>
+    //     <List
+    //       dataArray={pokemon.abilities}
+    //       renderRow={(item) => (
+    //         <ListItem>
+    //           <Text>{item.ability.name}</Text>
+    //         </ListItem>
+    //       )}
+    //     ></List>
+    //   </View>
+    // </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.header}>
+        #{pokemon.id} - {pokemon.name.toUpperCase()}
+      </Text>
+      <View style={styles.view}>
+        <Image
+          source={{ uri: pokemon.sprites.front_default }}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.info}>
+        <FlatList
+          ListHeaderComponent={
+            <>
+              <ListItem itemDivider>
+                <Text style={styles.infoText}>Size</Text>
+              </ListItem>
+              <ListItem>
+                <Text>Weight - {pokemon.weight}kg</Text>
+              </ListItem>
+              <ListItem>
+                <Text>Height - {pokemon.height / 10}m</Text>
+              </ListItem>
+              <ListItem itemDivider>
+                <Text style={styles.infoText}>Abilities</Text>
+              </ListItem>
+            </>
+          }
+          data={pokemon.abilities}
+          keyExtractor={(item, index) => "key" + index}
+          renderItem={({ item }) => (
+            <ListItem>
+              <Text>{item.ability.name}</Text>
+            </ListItem>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    fontSize: 30,
+    color: "red",
+    textAlign: "center",
+  },
+  view: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  image: {
+    height: 200,
+    width: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  info: {
+    flex: 1,
+    backgroundColor: "white",
+    opacity: 0.8,
+  },
+  infoText: {
+    fontWeight: "bold",
+  },
+});
